794c50f3fe6884b8a1b4ef50dcf7b8d208f2d151
Justin Hutchings <jhutchings1@users.noreply.github.com>
2021-01-11 11:07:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function endsWith(x, y) {
   let index = x.lastIndexOf(y);
   return index !== -1 && index === x.length - y.length;
 }
+
+function f() {
+	if (someCond());
+		return 23;
+	return 42;
+}
